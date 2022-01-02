d1d87f0b23897480b1b3a16bb91795025aae5963
Carl <casteeleu@gmail.com>
2022-01-02 18:55:42 -0500

    changed functionality of quiz including score keeper

--- script.js
+++ script.js
@@ -1,84 +1,66 @@
 const numOfQues = 3;
 let score = 0;
 let showScore = document.createTextNode(score)
-let nextBtnClick = 0;
-let showQuestionTwo = document.getElementById('q-2').style.display = 'none'
-let showQuestionThree = document.getElementById('q-3').style.display = 'none'
-let showFinalScore = document.getElementById('final-score').style.display = 'none'
-let startOver = document.getElementById('start-over').style.display = 'none'
+let currentQuestion = 1;
 const nextButton = document.getElementById('next-button')
-const startButton = document.getElementById('start-over')
+const startOverButton = document.getElementById('start-over')
 nextButton.addEventListener('click',nextFunc)
-startButton.addEventListener('click',startFunc)
+startOverButton.addEventListener('click',startFunc)
+document.querySelector(`#q-${currentQuestion}`).style.display = 'block'
+document.querySelector(`#q-${currentQuestion + 1}`).style.display = 'none'
+document.getElementById('final-score').style.display = 'none'
+startOverButton.style.display = 'none'
 
 function startFunc()
 {
     window.location.reload()
 }
 
-    console.log(score)
 
 function nextFunc()
   {
-    document.getElementById('q-1').style.display = 'block'
-    nextBtnClick += 1
     
-    if(nextBtnClick === 1)
-      {
-        document.getElementById('q-1').style.display = 'none'  
-        document.getElementById('q-2').style.display = 'block'
-      }
+    const correctAnswers = [document.getElementById('cones-of-dun').checked, 
+          document.getElementById('jjs-diner').checked,
+          document.getElementById('tammy').checked];
 
-      
-
-    
-    if(nextBtnClick === 2)
-      {
-        document.getElementById('q-1').style.display = 'none';    
-        document.getElementById('q-2').style.display = 'none'; 
-        document.getElementById('q-3').style.display = 'block';
-      }
-
-      
-
-    if(nextBtnClick === 3)
-      {
-        document.getElementById('q-1').style.display = 'none';    
-        document.getElementById('q-2').style.display = 'none'; 
-        document.getElementById('q-3').style.display = 'none';
-        document.getElementById('next-button').style.display = 'none';
-        document.getElementById('final-score').style.display = 'block';
-
-
-      }
-      
-
-
-      if(document.getElementById('correct-1').checked)
-      {
-          score += 1
-      }
-
-      if(document.getElementById('correct-2').checked)
+        if(currentQuestion === numOfQues)
         {
-          score +=1
-          score -=1
+          
+          document.querySelector(`#q-${currentQuestion}`).style.display = 'none' 
+          document.getElementById('next-button').style.display = 'none'
+          document.getElementById('final-score').style.display = 'block'
+          startOverButton.style.display = 'block'
+          for(let i = 0; i < correctAnswers.length; i++)
+          {
+            if(correctAnswers[i] === true)
+            {
+              score++
+              
+            }
+          }
+          // console.log(score)
         }
+              
 
-      if(document.getElementById('correct-3').checked)
+   
+          
+        else
         {
-            score += 1
-            score -= 1
-        }
-        document.getElementById('start-over').style.display = 'block'
+          document.querySelector(`#q-${currentQuestion}`).style.display = 'none'  
+          document.querySelector(`#q-${currentQuestion + 1}`).style.display = 'block'
+          currentQuestion++         
 
-      
+        }  
+
         let points = document.querySelector(".points").innerText = score 
-        let maxPoints = document.querySelector(".max-points").innerText = "3"   
-  }
+        let maxPoints = document.querySelector(".max-points").innerText = "3" 
+        console.log(score)
+      }
 
-  
 
+      
+        
 
 
 
